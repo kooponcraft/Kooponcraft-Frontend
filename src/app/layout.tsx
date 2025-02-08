9c19c686458af926c26511c282623246dbce36e7
Bemdoo Maor <bemdoo.maor1@gmail.com>
2025-02-08 03:03:16 +0100

    feat: keep image work on stale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,33 @@
 import "@/styles/index.scss";
 import { Metadata } from "next";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 export const metadata: Metadata = {
   title: "Kooponcraft",
   description: "Welcome to Kooponcraft - a gamified NFT marketplace for coupons",
   keywords: ["coupons", "nfts", "blockchain"],
   authors: { name: "Kooponcraft Team" },
+  openGraph: {
+    type: "website",
+    url: `${baseUrl}`,
+    title: "Kooponcraft - NFT Marketplace",
+    description: "Welcome to Kooponcraft - a gamified NFT marketplace for coupons",
+    siteName: "Kooponcraft",
+    images: [
+      {
+        url: "/full_logo.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: `${baseUrl}`,
+    creator: "@BemdooMaor",
+    title: "Kooponcraft - NFT Marketplace",
+    description: "Welcome to Kooponcraft - a gamified NFT marketplace for coupons",
+    images: "/full_logo.png",
+  },
 }
 
 export default function RootLayout({
