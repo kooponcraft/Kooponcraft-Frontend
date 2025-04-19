92251ed1426d2d1efea77a78a95c74df54bff39e
Bemdoo Maor <bemdoo.maor1@gmail.com>
2025-04-19 09:33:28 +0100

    fix: handle invalid slug in create page with 404 error component

--- src/app/[slug]/create/page.tsx
+++ src/app/[slug]/create/page.tsx
@@ -1,15 +1,27 @@
 import CreateNew from "@/components/dashboard/create-new";
-import { notFound } from "next/navigation";
+import Error from "@/components/error";
 import React from "react";
 
 export async function generateMetadata({ params }: any) {
 	const { slug } = await params
+
+	if(slug.toLowerCase() !== "items" || slug.toLowerCase() !== "collection"){
+		return {
+			title: `404 - Kooponcraft`,
+		}
+	}
+
 	return {
 		title: `Create New | ${slug} - Kooponcraft`,
 	}
   }
 
-const index = () => {
+const index = async ({ params }: any) => {
+	const { slug } = await params
+
+	if(slug.toLowerCase() !== "items" || slug.toLowerCase() !== "collection"){
+		return <Error />
+	}
 	return (
 		<>
 			<CreateNew />
